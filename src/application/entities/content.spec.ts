8b78288c2e558c2a375f6214455381815493abea
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-12-13 21:04:40 -0300

    feat: describe for tests

--- src/application/entities/content.spec.ts
+++ src/application/entities/content.spec.ts
@@ -1,19 +1,21 @@
 import { Content } from './content';
 
-test('It should be able to create a notification content', () => {
-  const content = new Content('Você Recebeu uma solicitação de amizade');
+describe('Notification content', () => {
+  it('should be able to create a notification content', () => {
+    const content = new Content('Você Recebeu uma solicitação de amizade');
 
-  expect(content).toBeTruthy();
-});
+    expect(content).toBeTruthy();
+  });
 
-test('It not be should able to create a notification content with less than 5 characters', () => {
-  expect(() => {
-    new Content('aaa');
-  }).toThrow();
-});
+  it('not be should able to create a notification content with less than 5 characters', () => {
+    expect(() => {
+      new Content('aaa');
+    }).toThrow();
+  });
 
-test('It not be should able to create a notification content with more than 240 characters', () => {
-  expect(() => {
-    new Content('a'.repeat(241));
-  }).toThrow();
+  it('not be should able to create a notification content with more than 240 characters', () => {
+    expect(() => {
+      new Content('a'.repeat(241));
+    }).toThrow();
+  });
 });
