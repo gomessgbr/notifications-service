71ceee2f3bc3ada266e6ef1f906feb835fecf0e0
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-12-13 21:14:52 -0300

    fix: create notification test

--- src/application/entities/notification.spec.ts
+++ src/application/entities/notification.spec.ts
@@ -4,10 +4,11 @@ import { Notification } from './notification';
 describe('Notification content', () => {
   it('should be able to create a notification', () => {
     const notification = new Notification({
-      content: new Content('Nova solicitação de amizade),
+      content: new Content('Zica filme'),
       category: 'social',
-      recipientId: 'exemple recipient Id',
-      createAt: ''
+      recipientId: 'example-recipient-id',
     });
+
+    expect(notification).toBeTruthy();
   });
 });
